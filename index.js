5bbe1b66ec1d25804a6424da0c06d3ec5fbe1a70
bhavesh0206s <bhaveshsuthar0299@gmail.com>
2020-09-16 23:21:33 +0530

    completed all sections

---
+++ index.js
@@ -0,0 +1,15 @@
+  // document.addEventListener( 'DOMContentLoaded', function () {
+  //   let splideIdEle = document.querySelector('#portfolio');
+  //   const showSlide = () =>{
+  //     let widthWin = window.innerWidth;
+  //     if(widthWin > 600){  
+  //       splideIdEle.classList.add('splide')
+  //       new Splide( '.splide' ).mount();
+  //     }else{
+  //       splideIdEle.classList.add('portfolio')
+  //     }
+  //   }
+
+  //   window.addEventListener('resize', showSlide);
+    
+  // } );
