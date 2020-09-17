071a9ded5c3976d330b5c6c2c9e53386ee52387b
bhavesh0206s <bhaveshsuthar0299@gmail.com>
2020-09-17 23:42:18 +0530

    completed navbar

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-  // document.addEventListener( 'DOMContentLoaded', function () {
-  //   let splideIdEle = document.querySelector('#portfolio');
-  //   const showSlide = () =>{
-  //     let widthWin = window.innerWidth;
-  //     if(widthWin > 600){  
-  //       splideIdEle.classList.add('splide')
-  //       new Splide( '.splide' ).mount();
-  //     }else{
-  //       splideIdEle.classList.add('portfolio')
-  //     }
-  //   }
 
-  //   window.addEventListener('resize', showSlide);
+  window.addEventListener("load", function (event) {
     
-  // } );
+    let aboutEle = document.querySelector('.nav__link--about');
+    let homeEle = document.querySelector('.nav__link--home');
+    let contactEle = document.querySelector('.nav__link--contact');
+    let workEle = document.querySelector('.nav__link--work');
+    let resumeEle = document.querySelector('.nav__link--resume');
+
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    
+    if(page === 'index.html'){
+      homeEle.classList.add('nav_link_js')
+    }
+    if(page === 'about.html'){
+      aboutEle.classList.add('nav_link_js')
+    }
+    if(page === 'work.html'){
+      workEle.classList.add('nav_link_js')
+    }
+    if(page === 'contact.html'){
+      contactEle.classList.add('nav_link_js')
+    }
+    if(page === 'resume.html'){
+      resumeEle.classList.add('nav_link_js')
+    }
+
+ });
