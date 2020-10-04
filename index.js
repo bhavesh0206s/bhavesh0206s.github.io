e49e36f5cce38a701772908caeaf485dce6d839b
bhavesh0206s <bhaveshsuthar0299@gmail.com>
2020-10-04 19:56:28 +0530

    added svg name

--- index.js
+++ index.js
@@ -8,37 +8,38 @@
     let resumeEle = document.querySelector('.nav__link--resume');
     let navToggleEle = document.querySelector('.nav-toggle');
     let navEle = document.querySelector('.nav');
-    let hamburger = document.querySelectorAll('.hamburger')
-    var path = window.location.pathname;
-    var page = path.split("/").pop();
+    let hamburger = document.querySelectorAll('.hamburger');
+
+    let path = window.location.pathname;
+    let page = path.split("/").pop();
 
 
     window.addEventListener('scroll', () =>{
       let yheight = window.scrollY;
       document.body.classList.remove('nav__show');
       hamburger.forEach(element => {
-        element.classList.remove('hamburger__cross')
+        element.classList.remove('hamburger__cross');
       });
       if(yheight > 40){
-        navEle.classList.add('nav--color')
+        navEle.classList.add('nav--color');
       }else{
-        navEle.classList.remove('nav--color')
+        navEle.classList.remove('nav--color');
       }
     })
 
     navToggleEle.addEventListener('click', () =>{
       document.body.classList.toggle('nav__show');
       hamburger.forEach(element => {
-        element.classList.toggle('hamburger__cross')
+        element.classList.toggle('hamburger__cross');
       });
 
     })
 
     navEle.addEventListener('click', (ele) => {
-      navEle.classList.add('nav--color')
+      navEle.classList.add('nav--color');
     })
 
-    if(page === 'index.html'){
+    if(page === 'index.html' || path === '/'){
       homeEle.classList.add('nav_link_js');
     }
     if(page === 'about.html'){
