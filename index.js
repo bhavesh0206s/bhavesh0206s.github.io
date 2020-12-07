68ce8ac497a862e7f9fb71b30cca9a3757b1007d
bhavesh0206s <bhaveshsuthar0299@gmail.com>
2020-12-07 18:32:23 +0530

    added one more project

--- index.js
+++ index.js
@@ -1,58 +1,57 @@
+window.addEventListener("load", function (event) {
+  
+  let aboutEle = document.querySelector('.nav__link--about');
+  let homeEle = document.querySelector('.nav__link--home');
+  let contactEle = document.querySelector('.nav__link--contact');
+  let workEle = document.querySelector('.nav__link--work');
+  let resumeEle = document.querySelector('.nav__link--resume');
+  let navToggleEle = document.querySelector('.nav-toggle');
+  let navEle = document.querySelector('.nav');
+  let hamburger = document.querySelectorAll('.hamburger');
 
-  window.addEventListener("load", function (event) {
-    
-    let aboutEle = document.querySelector('.nav__link--about');
-    let homeEle = document.querySelector('.nav__link--home');
-    let contactEle = document.querySelector('.nav__link--contact');
-    let workEle = document.querySelector('.nav__link--work');
-    let resumeEle = document.querySelector('.nav__link--resume');
-    let navToggleEle = document.querySelector('.nav-toggle');
-    let navEle = document.querySelector('.nav');
-    let hamburger = document.querySelectorAll('.hamburger');
-
-    let path = window.location.pathname;
-    let page = path.split("/").pop();
+  let path = window.location.pathname;
+  let page = path.split("/").pop();
 
 
-    window.addEventListener('scroll', () =>{
-      let yheight = window.scrollY;
-      document.body.classList.remove('nav__show');
-      hamburger.forEach(element => {
-        element.classList.remove('hamburger__cross');
-      });
-      if(yheight > 40){
-        navEle.classList.add('nav--color');
-      }else{
-        navEle.classList.remove('nav--color');
-      }
-    })
-
-    navToggleEle.addEventListener('click', () =>{
-      document.body.classList.toggle('nav__show');
-      hamburger.forEach(element => {
-        element.classList.toggle('hamburger__cross');
-      });
-
-    })
-
-    navEle.addEventListener('click', (ele) => {
+  window.addEventListener('scroll', () =>{
+    let yheight = window.scrollY;
+    document.body.classList.remove('nav__show');
+    hamburger.forEach(element => {
+      element.classList.remove('hamburger__cross');
+    });
+    if(yheight > 40){
       navEle.classList.add('nav--color');
-    })
+    }else{
+      navEle.classList.remove('nav--color');
+    }
+  })
 
-    if(page === 'index.html' || path === '/'){
-      homeEle.classList.add('nav_link_js');
-    }
-    if(page === 'about.html'){
-      aboutEle.classList.add('nav_link_js');
-    }
-    if(page === 'work.html'){
-      workEle.classList.add('nav_link_js');
-    }
-    if(page === 'contact.html'){
-      contactEle.classList.add('nav_link_js');
-    }
-    if(page === 'resume.html'){
-      resumeEle.classList.add('nav_link_js');
-    }
+  navToggleEle.addEventListener('click', () =>{
+    document.body.classList.toggle('nav__show');
+    hamburger.forEach(element => {
+      element.classList.toggle('hamburger__cross');
+    });
 
- });
+  })
+
+  navEle.addEventListener('click', (ele) => {
+    navEle.classList.add('nav--color');
+  })
+
+  if(page === 'index.html' || path === '/'){
+    homeEle.classList.add('nav_link_js');
+  }
+  if(page === 'about.html'){
+    aboutEle.classList.add('nav_link_js');
+  }
+  if(page === 'work.html'){
+    workEle.classList.add('nav_link_js');
+  }
+  if(page === 'contact.html'){
+    contactEle.classList.add('nav_link_js');
+  }
+  if(page === 'resume.html'){
+    resumeEle.classList.add('nav_link_js');
+  }
+
+});
